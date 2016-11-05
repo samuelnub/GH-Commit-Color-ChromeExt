16f127135a7523ecaefbf078a52d566d3a815ab3
samuelnub <notsamnub@gmail.com>
2016-11-05 11:01:02 +0000

    usable at this stage lol
    may work on changing the bars

--- js/content.js
+++ js/content.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     const calenderSvg = document.getElementsByClassName("js-calendar-graph-svg").item(0);
     if (!calenderSvg) {
         console.log("Can't get svg element! I guess this isn't a user's profile page :(");
@@ -22,8 +22,8 @@
         originalLegend[i] = legendElements[i].style.backgroundColor;
     }
 
-    (function() {
-        chrome.storage.sync.set({"originalLegend" : originalLegend}, function() {
+    (function () {
+        chrome.storage.sync.set({ "originalLegend": originalLegend }, function () {
             console.log("Sync'd original legend from the site itself! Here:");
             console.log(originalLegend);
         });
@@ -32,47 +32,49 @@
     const changeColor = function (changes, namespace) {
         chrome.storage.sync.get("ourLegend", function (items) {
             const newLegend = items["ourLegend"];
-            
-            if(!newLegend[0] && !newLegend[1] && !newLegend[2] && !newLegend[3] && !newLegend[4]) {
+
+            if (!newLegend[0] && !newLegend[1] && !newLegend[2] && !newLegend[3] && !newLegend[4]) {
                 let alreadySameCount = 0;
-                for(let i = 0; i < Object.keys(originalLegend).length; i++) {
-                    if(legendElements[i].style.backgroundColor == originalLegend[i]) {
+                for (let i = 0; i < Object.keys(originalLegend).length; i++) {
+                    if (legendElements[i].style.backgroundColor == originalLegend[i]) {
                         alreadySameCount++;
                     }
                 }
-                if(alreadySameCount == Object.keys(originalLegend).length) {
+                if (alreadySameCount == Object.keys(originalLegend).length) {
                     // Everything's the same, and we've recieved a completely undefined object (which means it was reset)
                     return;
                 }
             }
 
-            for (let i = 0; i < legendElements.length; i++) {
-                legendElements[i].setAttribute("style", "background-color: " + (newLegend[i] ? newLegend[i] : originalLegend[i]));
-            }
-
-            // of loop grabs the object as a whole
-            for (let square of calenderSquares) {
-                switch (square.val) {
-                    case originalLegend[0]:
-                        square.element.setAttribute("fill", (newLegend[0] ? newLegend[0] : originalLegend[0]));
-                        break;
-                    case originalLegend[1]:
-                        square.element.setAttribute("fill", (newLegend[1] ? newLegend[1] : originalLegend[1]));
-                        break;
-                    case originalLegend[2]:
-                        square.element.setAttribute("fill", (newLegend[2] ? newLegend[2] : originalLegend[2]));
-                        break;
-                    case originalLegend[3]:
-                        square.element.setAttribute("fill", (newLegend[3] ? newLegend[3] : originalLegend[3]));
-                        break;
-                    case originalLegend[4]:
-                        square.element.setAttribute("fill", (newLegend[4] ? newLegend[4] : originalLegend[4]));
-                        break;
-                    default:
-                        console.log("Couldn't find a colour match for square and legend :( " + square.val);
-                        break;
+            (function () {
+                for (let i = 0; i < legendElements.length; i++) {
+                    legendElements[i].setAttribute("style", "background-color: " + (newLegend[i] ? newLegend[i] : originalLegend[i]));
                 }
-            }
+
+                // of loop grabs the object as a whole
+                for (let square of calenderSquares) {
+                    switch (square.val) {
+                        case originalLegend[0]:
+                            square.element.setAttribute("fill", (newLegend[0] ? newLegend[0] : originalLegend[0]));
+                            break;
+                        case originalLegend[1]:
+                            square.element.setAttribute("fill", (newLegend[1] ? newLegend[1] : originalLegend[1]));
+                            break;
+                        case originalLegend[2]:
+                            square.element.setAttribute("fill", (newLegend[2] ? newLegend[2] : originalLegend[2]));
+                            break;
+                        case originalLegend[3]:
+                            square.element.setAttribute("fill", (newLegend[3] ? newLegend[3] : originalLegend[3]));
+                            break;
+                        case originalLegend[4]:
+                            square.element.setAttribute("fill", (newLegend[4] ? newLegend[4] : originalLegend[4]));
+                            break;
+                        default:
+                            console.log("Couldn't find a colour match for square and legend :( " + square.val);
+                            break;
+                    }
+                }
+            })();
         });
     };
     // Can't have iife and pass it to the listener callback, well at least not that i know of :(
